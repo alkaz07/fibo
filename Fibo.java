bd29943b27e1346a164fc0518856da4e3eb0fabc
alkaz07 <alkaz07@yandex.ru>
2022-11-15 19:52:29 +0300

    добивили "наивное" рекурсивное решение

--- Fibo.java
+++ Fibo.java
@@ -1,30 +1,39 @@
 import java.util.Scanner;
 
 public class Fibo {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int n;
         Scanner scan = new Scanner(System.in);
         System.out.print("Порядковый номер числа Фибоначчи: ");
         n = scan.nextInt();
 
-        int x =   fibonacci(n);
-        System.out.println("Это число: " +x);
+        int x = fibonacci(n);
+        System.out.println("Это число: " + x);
+        x = fibRecurcive(n);
+        System.out.println("Это число: " + x);
     }
 
     static int fibonacci(int n) {
         int f1, f2, f_n;
         f_n = 0;
-        if (n == 1 )
+        if (n == 1)
             f_n = 1;
         f1 = 0;
         f2 = 1;
         for (int i = 2; i <= n; i++) {
-                f_n = f1 + f2;
-                f1 = f2;
-                f2 = f_n;
+            f_n = f1 + f2;
+            f1 = f2;
+            f2 = f_n;
         }
 
         return f_n;
+    }
+
+    static int fibRecurcive(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return fibRecurcive(n-1) + fibRecurcive(n-2);
     }
 }
